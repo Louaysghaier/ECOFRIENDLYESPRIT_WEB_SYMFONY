80127a06964e09ea8571f23c5f50197e5be3b847
Louaysghaier <louay.sghaier@esprit.tn>
2023-11-25 23:08:25 +0100

    finalpush1

---
+++ src/Controller/OrdersController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Orders;
+use App\Entity\Service;
+use App\Entity\User2;
+use App\Form\Orders1Type;
+use App\Repository\OrdersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CurrentUserService;
+
+#[Route('/orders')]
+class OrdersController extends AbstractController
+{
+    #[Route('/pannier', name: 'app_orders_index', methods: ['GET'])]
+    public function index(OrdersRepository $ordersRepository): Response
+    {
+        return $this->render('orders/index.html.twig', [
+            'orders' => $ordersRepository->findAll(),
+        ]);
+    }
+
+  /*  #[Route('/shop', name: 'app_orders_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Fetch services from the database
+        $services = $this->getDoctrine()->getRepository(Service::class)->findAll();
+
+        return $this->render('service/ServicesShop.html.twig', [
+            'services' => $services,
+        ]);
+    }*/
+
+    #[Route('/{orderid}', name: 'app_orders_show', methods: ['GET'])]
+    public function show(Orders $order): Response
+    {
+        return $this->render('orders/show.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    #[Route('/{orderid}/edit', name: 'app_orders_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Orders $order, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(Orders1Type::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_orders_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('orders/edit.html.twig', [
+            'order' => $order,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{orderid}', name: 'app_orders_delete', methods: ['POST'])]
+    public function delete(Request $request, Orders $order, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$order->getOrderid(), $request->request->get('_token'))) {
+            $entityManager->remove($order);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_orders_index', [], Response::HTTP_SEE_OTHER);
+    }
+    private $currentUserService;
+
+    public function __construct(CurrentUserService $currentUserService)
+    {
+        $this->currentUserService = $currentUserService;
+    }
+/**
+     * Generate a random number with 4 digits and 2 characters.
+     *
+     * @return string
+     */
+    private function generateRandomNumOrder(): string
+    {
+        $numbers = str_pad(mt_rand(1, 9999), 4, '0', STR_PAD_LEFT);
+        $characters = chr(rand(65, 90)) . chr(rand(65, 90));
+
+        return $numbers . $characters;
+    }
+     #[Route("/{add-to-order}", name:'add_to_order', methods: ['POST'])]
+
+    public function addToOrder(Request $request): Response
+    {
+        $serviceId = $request->request->get('serviceId');
+
+        // Fetch the Service entity from the database using $serviceId
+        $service = $this->getDoctrine()->getRepository(Service::class)->find($serviceId);
+
+        if (!$service) {
+            return new JsonResponse(['success' => false, 'message' => 'Service not found.']);
+        }
+        // Get the current user from the CurrentUserService
+       // $user = $this->currentUserService->getCurrentUser();
+       $entityManager = $this->getDoctrine()->getManager();
+
+        $user = $entityManager->getRepository(User2::class)->findOneBy([]);
+
+        if (!$user instanceof User2) {
+            return new JsonResponse(['success' => false, 'message' => 'User not found.']);
+        }
+        $order = new Orders();
+        $order->setUserid($user);
+        $order->addServiceid($service); // Associate the service with the order
+        $order->setServices($service);
+
+        $order->setNumOrder($this->generateRandomNumOrder());
+        $order->setPhonenumber("55125290");
+        $order->setPriceorder($service->getPrice());
+        $order->setServices($service);
+
+        // Persist and flush the entities
+        $entityManager->persist($order);
+        
+        try {
+            $entityManager->flush();
+            
+            // If we reach this point, the order was successfully created
+            $orderData = [
+                'orderId' => $order->getOrderid(),
+                'numOrder' => $order->getNumOrder(),
+                'pickupdatetime' => $order->getPickupdatetime()->format('Y-m-d H:i:s'),
+                'status' => $order->getStatus(),
+                'phonenumber' => $order->getPhonenumber(),
+                'priceorder' => $order->getPriceorder(),
+                'user' => [
+                    'userId' => $order->getUserid()->getIduser(),
+                    'username' => $order->getUserid()->getNomuser(),
+                    // Add other user details as needed
+                ],
+                'services' => [
+                    [
+                        'serviceId' => $service->getServiceid(),
+                        'serviceName' => $service->getServicename(),
+                        // Add other service details as needed
+                    ]
+                    // You can add more services if the order involves multiple services
+                ],
+            ];
+        
+            return new JsonResponse(['success' => true, 'message' => 'Service added to order.', 'order' => $orderData]);
+        } catch (\Exception $e) {
+            // Handle exceptions, log them, or return an error response
+            return new JsonResponse(['success' => false, 'message' => 'Failed to create order.']);
+        }
+    }    
+}
---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Service;
+use App\Form\servicemodiftype;
+use App\Form\ServiceType;
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+#[Route('/service')]
+class ServiceController extends AbstractController
+{
+    #[Route('/', name: 'app_service_index', methods: ['GET'])]
+    public function index(ServiceRepository $serviceRepository): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'services' => $serviceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_service_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $service = new Service();
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('img')->getData();
+
+            if ($imageFile) {
+                $newFilename = uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('images_directory'), // Set your actual images directory
+                    $newFilename
+                );
+                $service->setImg($newFilename);
+            }
+
+            $entityManager->persist($service);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_service_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('service/new.html.twig', [
+            'service' => $service,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/edit/{serviceid}', name: 'app_service_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        $oldImg = $service->getImg(); // Get the existing image filename
+    
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('img')->getData();
+    
+            if ($imageFile) {
+                $newFilename = uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('images_directory'), // Set your actual images directory
+                    $newFilename
+                );
+                $service->setImg($newFilename);
+            } else {
+                // If no new image is provided, keep the old image
+                $service->setImg($oldImg);
+            }
+    
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('app_service_index', [], Response::HTTP_SEE_OTHER);
+        }
+    
+        return $this->renderForm('service/edit.html.twig', [
+            'service' => $service,
+            'form' => $form,
+            'oldImg' => $oldImg, // Pass the old image filename to the template
+        ]);
+    }
+    
+
+
+    #[Route('/{serviceid}', name: 'app_service_delete', methods: ['POST'])]
+    public function delete(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $service->getServiceid(), $request->request->get('_token'))) {
+            $entityManager->remove($service);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_service_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/show/{serviceid}', name: 'app_service_show', methods: ['GET'])]
+    public function show(Service $service): Response
+    {
+        return $this->render('service/show.html.twig', [
+            'service' => $service,
+        ]);
+    }
+    #[Route('/details/{serviceid}', name: 'app_orders_show_details', methods: ['GET'])]
+    public function showdetails(Service $service): Response
+    {
+        return $this->render('service/show_details.html.twig', [
+            'service' => $service,
+        ]);
+    }
+    #[Route('/shop', name: 'app_service_shop', methods: ['GET', 'POST'])]
+    public function homeshop(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Fetch services from the database
+        $services = $this->getDoctrine()->getRepository(Service::class)->findAll();
+
+        return $this->render('service/ServicesShop.html.twig', [
+            'services' => $services,
+        ]);
+    }
+
+    #[Route("/search", name: "search", methods: ['GET'])]
+public function search(Request $request): JsonResponse
+{
+    $serviceName = $request->query->get('serviceName');
+
+    // Perform your search logic here and retrieve the services
+    $services = $this->getDoctrine()->getRepository(Service::class)->searchByName($serviceName);
+
+    // Render the services as HTML using the same Twig template
+    $html = $this->renderView('service/ServicesShop.html.twig', ['services' => $services]);
+
+    // Check if there are search results
+    if (empty($services)) {
+        alert("Service doesnot exist ");
+    }
+
+    // Return a valid JSON response with HTML and no message
+    return new JsonResponse(['html' => $html]);
+}
+
+}
